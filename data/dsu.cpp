3e7566673ea0b54f882c36ad835ea953a81ace4d
brarie <52105387+brarie@users.noreply.github.com>
2023-03-13 10:27:12 +0900

    dsu second version
    dsu update

--- data/dsu.cpp
+++ data/dsu.cpp
@@ -1,19 +1,26 @@
 struct dsu {
-	int n;
-	vector<int> p;
+    int n;
+    vector<int> p;
 
-	dsu(int _n) : n(_n) {
-		p.resize(n);
-		iota(all(p), 0);
-	}
+    dsu(int _n) : n(_n) {
+        p.resize(n);
+        iota(p.begin(), p.end(), 0);
+    }
 
-	inline int find(int x) {
-		return (x == p[x] ? x : (p[x] = find(p[x])));
-	}
+    inline int find(int x) {
+        return (x == p[x] ? x : (p[x] = find(p[x])));
+    }
 
-	inline void merge(int u, int v) {
-		u = find(u), v = find(v);
-		if (u == v) return;
-		p[u] = v;
-	}
+    inline bool merge(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) {
+            return false;
+        }
+        p[u] = v;
+        return true;
+    }
+
+    inline bool same(int u, int v) {
+        return (find(u) == find(v));
+    }
 };
